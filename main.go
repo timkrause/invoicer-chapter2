87debcf9cac8bdf64ea71b86961380265ed296bf
Tim <timkrausemail@gmail.com>
2020-02-25 14:09:04 +0100

    5th time

--- main.go
+++ main.go
@@ -30,13 +30,10 @@ import (
 	"golang.org/x/oauth2"
 )
 
-
-
 type invoicer struct {
 	db    *gorm.DB
 	store *gormstore.Store
 }
-
 
 func main() {
 	var (
@@ -100,7 +97,6 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-
 type Invoice struct {
 	gorm.Model
 	IsPaid      bool      `json:"is_paid"`
@@ -126,34 +122,32 @@ func (iv *invoicer) getInvoice(w http.ResponseWriter, r *http.Request) {
 	iv.db.First(&i1, id)
 	fmt.Printf("%+v\n", i1)
 	if i1.ID == 0 {
-		httpError(w, r, http.StatusNotFound, "No invoice id %s", vars["id"])
+		httpError(w, http.StatusNotFound, "No invoice id %s", vars["id"])
 		return
 	}
 	iv.db.Where("invoice_id = ?", i1.ID).Find(&i1.Charges)
 	jsonInvoice, err := json.Marshal(i1)
 	if err != nil {
-		httpError(w, r, http.StatusInternalServerError, "failed to retrieve invoice id %d: %s", vars["id"], err)
+		httpError(w, http.StatusInternalServerError, "failed to retrieve invoice id %d: %s", vars["id"], err)
 		return
 	}
 	w.Header().Add("Content-Type", "application/json")
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonInvoice)
-	al := appLog{Message: fmt.Sprintf("retrieved invoice %d", i1.ID), Action: "get-invoice"}
-	al.log(r)
 }
 
 func (iv *invoicer) postInvoice(w http.ResponseWriter, r *http.Request) {
 	log.Println("posting new invoice")
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		httpError(w, r, http.StatusBadRequest, "failed to read request body: %s", err)
+		httpError(w, http.StatusBadRequest, "failed to read request body: %s", err)
 		return
 	}
 	var i1 Invoice
 	err = json.Unmarshal(body, &i1)
 	if err != nil {
-		httpError(w, r, http.StatusBadRequest, "failed to parse request body: %s", err)
+		httpError(w, http.StatusBadRequest, "failed to parse request body: %s", err)
 		return
 	}
 	// make sure the IDs are null before inserting
@@ -164,10 +158,9 @@ func (iv *invoicer) postInvoice(w http.ResponseWriter, r *http.Request) {
 	}
 	iv.db.Create(&i1)
 	iv.db.Last(&i1)
+	log.Printf("%+v\n", i1)
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("created invoice %d", i1.ID)))
-	al := appLog{Message: fmt.Sprintf("created invoice %d", i1.ID), Action: "post-invoice"}
-	al.log(r)
 }
 
 func (iv *invoicer) putInvoice(w http.ResponseWriter, r *http.Request) {
@@ -176,17 +169,17 @@ func (iv *invoicer) putInvoice(w http.ResponseWriter, r *http.Request) {
 	var i1 Invoice
 	iv.db.First(&i1, vars["id"])
 	if i1.ID == 0 {
-		httpError(w, r, http.StatusNotFound, "No invoice id %s", vars["id"])
+		httpError(w, http.StatusNotFound, "No invoice id %s", vars["id"])
 		return
 	}
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		httpError(w, r, http.StatusBadRequest, "failed to read request body: %s", err)
+		httpError(w, http.StatusBadRequest, "failed to read request body: %s", err)
 		return
 	}
 	err = json.Unmarshal(body, &i1)
 	if err != nil {
-		httpError(w, r, http.StatusBadRequest, "failed to parse request body: %s", err)
+		httpError(w, http.StatusBadRequest, "failed to parse request body: %s", err)
 		return
 	}
 	iv.db.Save(&i1)
@@ -194,17 +187,15 @@ func (iv *invoicer) putInvoice(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%+v\n", i1)
 	w.WriteHeader(http.StatusAccepted)
 	w.Write([]byte(fmt.Sprintf("updated invoice %d", i1.ID)))
-	al := appLog{Message: fmt.Sprintf("updated invoice %d", i1.ID), Action: "put-invoice"}
-	al.log(r)
 }
 
 func (iv *invoicer) deleteInvoice(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
 	if !checkCSRFToken(r.Header.Get("X-CSRF-Token")) {
 		w.WriteHeader(http.StatusNotAcceptable)
 		w.Write([]byte("Invalid CSRF Token"))
 		return
 	}
-	vars := mux.Vars(r)
 	log.Println("deleting invoice", vars["id"])
 	var i1 Invoice
 	id, _ := strconv.Atoi(vars["id"])
@@ -213,13 +204,11 @@ func (iv *invoicer) deleteInvoice(w http.ResponseWriter, r *http.Request) {
 	iv.db.Delete(&i1)
 	w.WriteHeader(http.StatusAccepted)
 	w.Write([]byte(fmt.Sprintf("deleted invoice %d", i1.ID)))
-	al := appLog{Message: fmt.Sprintf("deleted invoice %d", i1.ID), Action: "delete-invoice"}
-	al.log(r)
 }
 
 func (iv *invoicer) getIndex(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Security-Policy", "default-src 'self';")
-	log.Println("serving index page")
+	w.Header().Add("Content-Security-Policy", "default-src 'self'; child-src 'self;")
+	w.Header().Add("X-Frame-Options", "SAMEORIGIN")
 	w.Write([]byte(`
 <!DOCTYPE html>
 <html>
@@ -231,6 +220,8 @@ func (iv *invoicer) getIndex(w http.ResponseWriter, r *http.Request) {
     </head>
     <body>
 	<h1>Invoicer Web</h1>
+	<p><a href="/authenticate">Authenticate with Google</a></p>
+	</p>
         <p class="desc-invoice"></p>
         <div class="invoice-details">
         </div>
@@ -238,6 +229,7 @@ func (iv *invoicer) getIndex(w http.ResponseWriter, r *http.Request) {
         <form id="invoiceGetter" method="GET">
             <label>ID :</label>
             <input id="invoiceid" type="text" />
+            <input type="hidden" name="CSRFToken" value="` + makeCSRFToken() + `">
             <input type="submit" />
         </form>
         <form id="invoiceDeleter" method="DELETE">
@@ -247,6 +239,28 @@ func (iv *invoicer) getIndex(w http.ResponseWriter, r *http.Request) {
     </body>
 </html>`))
 }
+
+func getHeartbeat(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("I am alive"))
+}
+
+// handleVersion returns the current version of the API
+func getVersion(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf(`{
+"source": "https://github.com/Securing-DevOps/invoicer",
+"version": "%s",
+"commit": "%s",
+"build": "https://circleci.com/gh/Securing-DevOps/invoicer/"
+}`, version, commit)))
+}
+
+func httpError(w http.ResponseWriter, errorCode int, errorMessage string, args ...interface{}) {
+	log.Printf("%d: %s", errorCode, fmt.Sprintf(errorMessage, args...))
+	http.Error(w, fmt.Sprintf(errorMessage, args...), errorCode)
+	return
+}
+
+var CSRFKey []byte
 
 func makeCSRFToken() string {
 	msg := make([]byte, 32)
@@ -269,17 +283,75 @@ func checkCSRFToken(token string) bool {
 	return hmac.Equal(messageMAC, expectedMAC)
 }
 
-
-func getHeartbeat(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("I am alive"))
+var oauthCfg = &oauth2.Config{
+	ClientID:     "606479880714-v36tg6qtn9alsinbvfb0qtmvjdkunq4c.apps.googleusercontent.com",
+	ClientSecret: "ySBC6T-F31ez3qsA3lnNRvtr",
+	RedirectURL:  "http://localhost:8080/oauth2callback",
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.profile"},
+	Endpoint: oauth2.Endpoint{
+		AuthURL:  "https://accounts.google.com/o/oauth2/auth",
+		TokenURL: "https://accounts.google.com/o/oauth2/token",
+	},
 }
 
-// handleVersion returns the current version of the API
-func getVersion(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf(`{
-"source": "https://github.com/Securing-DevOps/invoicer",
-"version": "%s",
-"commit": "%s",
-"build": "https://circleci.com/gh/Securing-DevOps/invoicer/"
-}`, version, commit)))
+func (iv *invoicer) getAuthenticate(w http.ResponseWriter, r *http.Request) {
+	//Get the Google URL which shows the Authentication page to the user
+	url := oauthCfg.AuthCodeURL(makeCSRFToken())
+	//redirect user to that page
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+// Function that handles the callback from the IDP
+func (iv *invoicer) getOAuth2Callback(w http.ResponseWriter, r *http.Request) {
+	if !checkCSRFToken(r.FormValue("state")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write([]byte("Failed to verify oauth state via CSRF token '" + r.FormValue("state") + "'"))
+		return
+	}
+	token, err := oauthCfg.Exchange(oauth2.NoContext, r.FormValue("code"))
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write([]byte("Failed to obtain token from oauth code " + r.FormValue("code")))
+		return
+	}
+
+	client := oauthCfg.Client(oauth2.NoContext, token)
+	resp, err := client.Get(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json`)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to retrieve user information from IDP"))
+		return
+	}
+	buf := make([]byte, 10240)
+	resp.Body.Read(buf)
+	fmt.Printf("%s\n", buf)
+	var up UserProfile
+	//err = json.Unmarshal(buf, &up)
+	//if err != nil {
+	//	w.WriteHeader(http.StatusExpectationFailed)
+	//	w.Write([]byte("Failed to parse user information from " + string(buf)))
+	//	return
+	//}
+
+	// Create a session, save it and return a cookie
+	session, err := iv.store.Get(r, "session")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to create session for user"))
+		return
+	}
+	sid := makeCSRFToken()
+	session.Values[up.Name] = sid
+	iv.store.Save(r, w, session)
+
+	w.Write([]byte(fmt.Sprintf(`<html><body>
+This app is now authenticated to access your Google user info.  Your details are:<br />
+%s
+<img src="%s" />
+</body></html>`, up.Name, up.Picture)))
+}
+
+type UserProfile struct {
+	Name    string `json:"name"`
+	Picture string `json:"picture"`
 }
